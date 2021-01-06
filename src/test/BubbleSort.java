cf8a810eca04d4fcd802970702fe410ba64b6328
AL01570777\NHN <sjyim84@gmail.com>
2021-01-07 01:12:46 +0900

    commit uncompleted sort method code

--- src/test/BubbleSort.java
+++ src/test/BubbleSort.java
@@ -2,5 +2,14 @@ package test;
 
 public class BubbleSort {
 	private int[] arr = {9, 6, 1, 3, 5, 2, 8, 7, 4};
+	
+//	{ 1, 3, 2, 5, 6, 8, 7, 4, 9}
+	
+	void sort() {
+		for (int i = 0; i < arr.length; i++) {
+			
+		}
+		System.out.println(arr);
+	}
 
 }
