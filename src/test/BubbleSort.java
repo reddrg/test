8297a9fb6ad947b92d02bb008333fa0deed3ed66
강두영 <dooykang@smilegate.com>
2021-01-07 01:10:34 +0900

    complete bubbleSort

--- src/test/BubbleSort.java
+++ src/test/BubbleSort.java
@@ -1,6 +1,25 @@
 package test;
 
 public class BubbleSort {
-	private int[] arr = {9, 6, 1, 3, 5, 2, 8, 7, 4};
-
+	private static final int[] ARR = {9, 6, 1, 3, 5, 2, 8, 7, 4};
+	
+	public static int[] bubbleSort(int[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			for(int j = 0; j < arr.length - i; j++) {
+				int temp = 0;
+				if(arr[j] > arr[j+1]) {
+					temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] result = bubbleSort(ARR);
+		for(int temp : result)
+			System.out.print(temp + " ");
+	}
 }
